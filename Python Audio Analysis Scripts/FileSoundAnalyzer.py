cf9a496eb422cf4ba01ac991057fda408cdfbce8
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-09-12 21:21:34 -0400

    Files moved

---
+++ Python Audio Analysis Scripts/FileSoundAnalyzer.py
@@ -0,0 +1,203 @@
+import mutagen
+import wave
+import numpy as np
+import struct
+import subprocess
+import os
+#import soundfile as sf
+#import matplotlib.pyplot as plt
+import speech_recognition as sr
+#from pocketsphinx import AudioFile, get_model_path, get_data_path
+from multiprocessing import Process
+import contextlib
+import specgram
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = '/tmp/'#os.path.join(APP_ROOT, 'static/uploads/')
+
+SECOND = 44100
+MINUTE = 60 * SECOND
+THREE_MINUTE = 2 * MINUTE
+sz = SECOND
+r = sr.Recognizer()
+
+class SoundAnalyzer(object):
+	def __init__(self, filename):
+		self.filename = filename
+		self.seconds = None
+
+	def create_input_wav_file(self):
+		#print os.path.dirname(os.path.abspath(__file__))
+		command_string = 'mpg123 -k 7000 -n 2400 -w input.wav {}'.format(UPLOAD_FOLDER + self.filename)
+		commands = command_string.split()
+		p = subprocess.Popen(commands, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		p.wait()
+
+	def duration(self):
+		with contextlib.closing(wave.open(UPLOAD_FOLDER + self.filename,'r')) as f:
+		    frames = f.getnframes()
+		    rate = f.getframerate()
+		    duration = frames / float(rate)
+		    print(duration)
+		    self.seconds = duration
+		    return int(duration)
+		# f = sf.SoundFile(UPLOAD_FOLDER + self.filename)
+		# print('samples = {}'.format(len(f)))
+		# print('sample rate = {}'.format(f.samplerate))
+		# self.seconds = (len(f) / f.samplerate)
+		# return self.seconds
+
+	def process_file(self, should_filter=False, preprocess = False, aslist = False):
+		wr = None
+		seconds = None
+		if preprocess:
+			#print 'Preprocessing'
+			self.create_input_wav_file()
+			wr = wave.open('input.wav', 'r')
+		else:
+			wr = wave.open(UPLOAD_FOLDER + self.filename, 'r')
+			seconds = self.duration()
+		sz = seconds * 60#SECOND # Read and process 1 second at a time, 44.1 kHz
+		print("Seconds of song")
+		print(seconds)
+		data = wr.readframes(sz)
+
+		# the wave api of doing things: 
+		channels = wr.getnchannels()
+		frame_data = struct.unpack('{}h'.format(sz * channels), data)
+		print('Frame data: ')
+		print(list(frame_data))
+	
+		da = np.fromstring(data, dtype=np.int16)
+		wr.close()
+		left, right = da[0::2], da[1::2]
+
+		# returns 1/2+1 as many numbers as it was given
+		lf, rf = np.fft.rfft(left), np.fft.rfft(right)
+
+		if should_filter:
+			#print 'Filter'
+			lowpass = 21 # Remove lower frequencies below human hearing range
+			highpass = 9000 # Remove higher frequencies above human voice range
+
+			lf[:lowpass], rf[:lowpass] = 0, 0 # low pass filter 
+			lf[55:66], rf[55:66] = 0, 0 # line noise filter
+			lf[highpass:], rf[highpass:] = 0,0 # high pass filter 
+			nl, nr = np.fft.irfft(lf), np.fft.irfft(rf) # we recreate the original signal via an inverse FFT
+
+			# combines the left and right channels again using the column_stack() function, interleaves the left and right samples using the ravel() method and finally converts them to 16-bit integers with the astype() method
+			ns = np.column_stack((nl,nr)).ravel().astype(np.int16)
+
+		if aslist:
+			return {'sound': { 'left':left.tolist(), 'right':right.tolist() }, 'frequency': {'left':lf.tolist(), 'right': rf.tolist()}}
+
+		print("Done processing audio file")
+
+		print(len(left))
+
+		# For now, not returning frequency - need to figure out how to deal with frequency
+		#return {'sound': { 'left':left, 'right':right }, 'frequency': {'left':lf, 'right': rf}}
+		return {'sound': { 'left':left.tolist(), 'right':right.tolist() }} #, 'frequency': {'left':lf, 'right': rf}}
+
+	def transcribe_file(self):
+		# use the audio file as the audio source
+		with sr.AudioFile(UPLOAD_FOLDER + self.filename) as source:
+			audio = r.record(source)  # read the entire audio file
+
+		#process_goog = Process(target=self.run_google_speech_rec, args=(audio,))
+		#process_goog.start()
+
+		# recognize speech using Google Speech Recognition
+		#self.run_google_speech_rec(audio)
+		'''
+		model_path = get_model_path()
+		data_path = get_data_path()
+
+		config = {
+		    'verbose': False,
+		    'audio_file': os.path.join(UPLOAD_FOLDER, 'castle.raw'),
+		    'buffer_size': 2048,
+		    'no_search': False,
+		    'full_utt': False,
+		    'hmm': os.path.join(model_path, 'en-us'),
+		    'lm': os.path.join(model_path, 'en-us.lm.bin'),
+		    'dict': os.path.join(model_path, 'cmudict-en-us.dict')
+		    }
+		audio = AudioFile(**config)
+		for phrase in audio:
+		    print(phrase)
+		'''
+
+
+	def run_google_speech_rec(self, audio):
+		try:
+			# write text fo file
+			file = open('googlespeech.txt', 'w+')
+			text = "Google Speech Recognition thinks you said: \n" + str(r.recognize_google(audio))
+			#print text
+			file.write(text)
+		except sr.UnknownValueError:
+		    print("Google Speech Recognition could not understand audio")
+		except sr.RequestError as e:
+		    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+	def run_sphinx_speech_rec(self, audio):
+		# recognize speech using Sphinx
+		try:
+		    return ("Sphinx thinks you said: \n" + r.recognize_sphinx(audio))
+		except sr.UnknownValueError:
+		    print("Sphinx could not understand audio")
+		except sr.RequestError as e:
+		    print("Sphinx error; {0}".format(e))
+
+	def run_wit_ai_speech_rec(self, wit_ai_key, audio):
+		# recognize speech using Wit.ai
+		try:
+			return ("Wit.ai thinks you said: \n" + r.recognize_wit(audio, key=wit_ai_key))
+		except sr.UnknownValueError:
+		    print("Wit.ai could not understand audio")
+		except sr.RequestError as e:
+		    print("Could not request results from Wit.ai service; {0}".format(e))
+
+	def run_bing_speech_rec(self, bing_key, audio):
+		# recognize speech using Microsoft Bing Voice Recognition
+		try:
+		    return ("Microsoft Bing Voice Recognition thinks you said: \n" + r.recognize_bing(audio, key=bing_key))
+		except sr.UnknownValueError:
+		    print("Microsoft Bing Voice Recognition could not understand audio")
+		except sr.RequestError as e:
+		    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+
+
+'''
+# for debugging, plot a figure of the sound and the frequencies 
+def graph_fft(sound_data, frequency_data, seconds):
+	plt.figure(1)
+	a = plt.subplot(211)
+	r = 2**16/2
+	a.set_ylim([-r, r])
+	a.set_xlabel('time [s]')
+	a.set_ylabel('sample value [-]')
+	x = np.arange(seconds)/(seconds)
+	plt.plot(x, sound_data)
+	b = plt.subplot(212)
+	b.set_xscale('log')
+	b.set_xlabel('frequency [Hz]')
+	b.set_ylabel('|amplitude|')
+	plt.plot(abs(frequency_data))
+	plt.savefig('sample-graph.png')
+'''
+
+
+
+
+if __name__ == '__main__':
+	sound = SoundAnalyzer("Burin_It_Down.wav")
+	#sound.transcribe_file()
+	data = sound.process_file()
+	#sound.duration()
+	#graph_fft(data['sound']['left'], data['frequency']['left'], sound.seconds * SECOND)
+
+
+
+
---
+++ Python Audio Analysis Scripts/specgram.py
@@ -0,0 +1,200 @@
+from __future__ import division
+import scipy.io.wavfile as wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+import xlrd
+import pandas as pd
+#import seaborn
+from scipy.signal import argrelextrema
+from scipy.interpolate import interp1d
+#import abjad
+import re
+
+NoteFile = pd.read_excel('NoteFreq.xlsx',0)
+#staff = abjad.Staff()
+
+debug = False
+
+def fixNotes(letters):
+
+    m = letters
+
+    if m[-1]=='0':
+        #note = m[0].upper()+ m[0].upper()+ m[0].upper()
+        note = m[0]+3*','
+
+    if m[-1]=='1':
+        #note = m[0].upper()+ m[0].upper()
+        note = m[0]+2*','
+
+    if m[-1]=='2':
+        note = m[0]+','
+
+    if m[-1]=='3':
+        note = m[0]
+
+    if m[-1]=='4':
+        note = m[0]+"'"
+
+    if m[-1]=='5':
+        note = m[0]+2*"'"
+
+    if m[-1]=='6':
+        note = m[0]+3*"'"
+
+    if m[-1]=='7':
+        note = m[0]+4*"'"
+
+    if m[-1]=='8':
+        note = m[0]+5*"'"
+
+    if m[-1]=='9':
+        note = m[0]+6*"'"
+
+    if m[-1]=='10':
+        note = m[0]+7*"'"
+
+    if m[1]=='s':
+        fixed = note
+        fixed = fixed[:1]+'s'+fixed[1:]
+    else:
+        fixed = note
+
+    return fixed
+
+class MusicNoteAnalyzer(object):
+    def __init__(self, filename):
+        self.filename = '/tmp/' + filename
+        self.rate, self.data = wavfile.read(self.filename)
+        #print self.rate
+        #print self.data[22100]
+
+    def generateGraphData(self):
+        if debug:
+            print(self.data[:,0])
+        time = np.arange(len(self.data[:,0]))*1.0/self.rate
+        nfft = 1024*6
+        pxx, freq, bins, plot = plt.specgram(self.data[:,0],NFFT=nfft,noverlap=32, Fs=2,
+                     interpolation='bilinear')
+
+        # Another plot
+        #plt.show()
+        plt.savefig('specgram1.png')
+        a = np.mean(pxx,axis=0)
+        aa = np.arange(len(a))
+        a = a/np.max(a)*np.max(self.data[:,0])
+        aa = aa/np.max(aa) * time[-1]
+
+        f = interp1d(aa,a)
+        newSmooth = f(time)
+
+        indMax = argrelextrema(newSmooth, np.greater)[0]
+        indMin = argrelextrema(newSmooth, np.less)[0]
+
+        lastValue = np.where(newSmooth==newSmooth[-1])[0]
+        indMin = np.hstack((indMin,lastValue))
+
+        # A plot
+        # plt.plot(time,self.data[:,0])
+        # plt.plot(aa,a)
+        # plt.plot(time[indMax],newSmooth[indMax])
+        # plt.plot(time[indMin],newSmooth[indMin])
+        #plt.show()
+        #plt.savefig('specgram2.png')
+
+        notes = np.array([indMax,indMin]).T
+        letterNotes, freqs, individualNotes = self.getFreq(notes, self.data)
+
+        if debug:
+            print("Letter notes: ")
+            print(letterNotes)
+
+        result = []
+        # Format the notes
+        for i,v in enumerate(letterNotes):
+            #print v
+            letters = letterNotes[i].encode('ASCII').lower()
+            try:
+                fixed = fixNotes(letters)
+                result.append(fixed)
+            except UnboundLocalError:
+                m = re.search('\w.\d',letters)
+                shortenedNote = m.group(0)
+                newNote = shortenedNote[0]+'s'+shortenedNote[-1]
+                fixed = fixNotes(newNote)
+                #print fixed
+                result.append(fixed)
+                pass
+
+        return result
+
+
+    def getHarmonics(self,p,f,maxPower,maxFrequency,harmonics,harm):
+        x = maxFrequency/harm
+        #problem is that its not exactly in f
+        ind1 = np.where(f<=x+1)
+        ind2 = np.where(f>=x-1)
+        mask = np.in1d(ind1,ind2)
+        index = np.where(mask == True)[0]
+
+        #print 'frequency'
+        #print f[index]
+        condition = p[index]/maxPower
+        try:
+            condition = condition[0]
+        except IndexError:
+            pass
+
+        #if p[index]/maxPower>=0.90:
+        if condition>=0.90:
+            harmonics.append(f[index][0])
+
+        return harmonics
+
+    def getFreq(self, notes, data):
+        individualNotes = []
+        freqs = []
+        letterNotes = []
+
+        for i,v in enumerate(notes):
+
+            individualNotes.append(data[v[0]:v[1],0])
+
+            p = 20*np.log10(np.abs(np.fft.rfft(data[v[0]:v[1], 0])))
+            f = np.linspace(0, self.rate/2.0, len(p))
+
+            harmonics = []
+            maxPower = np.max(p)
+            maxFrequency = f[np.where(p==max(p))][0]
+
+            for j in range(2,8):
+                harmonics = self.getHarmonics(p,f,maxPower,maxFrequency,harmonics,j)
+
+            if harmonics==[]:
+                harmonics = [maxFrequency]
+
+            #print 'HARMONICS'
+            #print harmonics
+
+    #        maxFFT = np.where(p==np.max(p))
+    #        maxFreq = f[maxFFT]
+    #        freqs.append(maxFreq)
+            maxFreq = harmonics
+    #        q = NoteFile['Lower']<maxFreq[0]
+    #        r = NoteFile['Upper']>maxFreq[0]
+    #        note = np.in1d(q,r)
+
+
+            a = NoteFile[NoteFile['Lower']<maxFreq[0]]
+            b = NoteFile[NoteFile['Upper']>maxFreq[0]]
+            note = a.join(b,how='inner',lsuffix='Lower').index[0]
+
+            letterNotes.append(note)
+
+        return letterNotes, freqs, individualNotes
+
+if __name__ == '__main__':
+    music = MusicNoteAnalyzer('static/uploads/Shelter.wav')
+    music.generateGraphData()
+
+
