1c9b28aa4ce9c585d4361c436e4a7ef6828e0173
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-11-27 15:06:19 -0500

    Successfully split between harmonic and percussive

--- pyAudioAnalysis/sourceSeparation.py
+++ pyAudioAnalysis/sourceSeparation.py
@@ -7,33 +7,43 @@ import librosa.display
 
 
 
-def main():
+def decomposeAudio(filename, PLOT = False):
 	print('Called main')
-	y, sr = librosa.load('data/mono/Shelter_mono_thumb1.wav', offset=40, duration=10)
+	y, sr = librosa.load(filename, offset=40, duration=10)
 	D = librosa.stft(y)
 	D_harmonic, D_percussive = librosa.decompose.hpss(D)
 	# Pre-compute a global reference power from the input spectrum
 	rp = np.max(np.abs(D))
 
-	print('Plotting spectogram')
-	plt.figure(figsize=(12, 8))
+	# time domain signal
+	D_harmonic_time = librosa.istft(D_harmonic)
+	D_percussive_time = librosa.istft(D_percussive)
 
-	plt.subplot(3, 1, 1)
-	librosa.display.specshow(librosa.amplitude_to_db(D, ref=rp), y_axis='log')
-	plt.colorbar()
-	plt.title('Full spectrogram')
+	librosa.output.write_wav('penguindrum_harmonic.wav', D_harmonic_time, sr)
+	librosa.output.write_wav('penguindrum_percussive.wav', D_percussive_time, sr)
 
-	plt.subplot(3, 1, 2)
-	librosa.display.specshow(librosa.amplitude_to_db(D_harmonic, ref=rp), y_axis='log')
-	plt.colorbar()
-	plt.title('Harmonic spectrogram')
+	if PLOT:
+		print('Plotting spectogram')
+		fig = plt.figure(figsize=(12, 8))
 
-	plt.subplot(3, 1, 3)
-	librosa.display.specshow(librosa.amplitude_to_db(D_percussive, ref=rp), y_axis='log', x_axis='time')
-	plt.colorbar()
-	plt.title('Percussive spectrogram')
-	plt.tight_layout()
-	plt.show()
+		plt.subplot(3, 1, 1)
+		librosa.display.specshow(librosa.amplitude_to_db(D, ref=rp), y_axis='log')
+		plt.colorbar()
+		plt.title('Full spectrogram')
+
+		plt.subplot(3, 1, 2)
+		librosa.display.specshow(librosa.amplitude_to_db(D_harmonic, ref=rp), y_axis='log')
+		plt.colorbar()
+		plt.title('Harmonic spectrogram')
+
+		plt.subplot(3, 1, 3)
+		librosa.display.specshow(librosa.amplitude_to_db(D_percussive, ref=rp), y_axis='log', x_axis='time')
+		plt.colorbar()
+		plt.title('Percussive spectrogram')
+		#plt.tight_layout()
+		#plt.show()
+		fig.savefig('data/graphs/penguindrumSpectrogram.png')
+		plt.show()
 
 	print('Computing decompositions')
 	# Let's compute separations for a few different margins and compare the results below
@@ -42,74 +52,37 @@ def main():
 	D_harmonic8, D_percussive8 = librosa.decompose.hpss(D, margin=8)
 	D_harmonic16, D_percussive16 = librosa.decompose.hpss(D, margin=16)
 
-	plt.figure(figsize=(10, 10))
-
 	harmonics = [D_harmonic, D_harmonic2, D_harmonic4, D_harmonic8, D_harmonic16]
-	percussion = [D_percussive, D_percussive2, D_percussive4, D_harmonic8, D_harmonic16]
+	percussion = [D_percussive, D_percussive2, D_percussive4, D_percussive8, D_percussive16]
 
-	for i, harm in enumerate(harmonics):
-		plt.subplot(5,2,i+1)
-		librosa.display.specshow(librosa.amplitude_to_db(harm, ref=rp), y_axis='log')
-		plt.title('Harmonic')
-		plt.yticks([])
-		plt.ylabel('margin=' + str(i+2))
+	if PLOT:
+		fig = plt.figure(figsize=(10, 10))
 
-	for i, perc in enumerate(percussion):
-		plt.subplot(5,2,i+5)
-		librosa.display.specshow(librosa.amplitude_to_db(perc, ref=rp), y_axis='log')
-		plt.title('Percussive')
-		plt.yticks([])
-		#plt.ylabel('margin=1')
+		for i, harm in enumerate(harmonics):
+			plt.subplot(5,2,2*i+1)
+			librosa.display.specshow(librosa.amplitude_to_db(harm, ref=rp), y_axis='log')
+			plt.title('Harmonic')
+			plt.yticks([])
+			plt.ylabel('margin=' + str(i+2))
 
-	# plt.subplot(5, 2, 1)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_harmonic, ref=rp), y_axis='log')
-	# plt.title('Harmonic')
-	# plt.yticks([])
-	# plt.ylabel('margin=1')
+			plt.subplot(5,2,2*i+2)
+			librosa.display.specshow(librosa.amplitude_to_db(percussion[i], ref=rp), y_axis='log')
+			plt.title('Percussive')
+			plt.yticks([])
 
-	# plt.subplot(5, 2, 2)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_percussive, ref=rp), y_axis='log')
-	# plt.title('Percussive')
-	# plt.yticks([]), plt.ylabel('')
+			print(str(2*i+1) + ',' + str(2*i+2))
 
-	# plt.subplot(5, 2, 3)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_harmonic2, ref=rp), y_axis='log')
-	# plt.yticks([])
-	# plt.ylabel('margin=2')
+		fig.savefig('data/graphs/penguindrumComponentsSpectrogram.png')
 
-	# plt.subplot(5, 2, 4)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_percussive2, ref=rp), y_axis='log')
-	# plt.yticks([]) ,plt.ylabel('')
-
-	# plt.subplot(5, 2, 5)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_harmonic4, ref=rp), y_axis='log')
-	# plt.yticks([])
-	# plt.ylabel('margin=4')
-
-	# plt.subplot(5, 2, 6)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_percussive4, ref=rp), y_axis='log')
-	# plt.yticks([]), plt.ylabel('')
-
-	# plt.subplot(5, 2, 7)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_harmonic8, ref=rp), y_axis='log')
-	# plt.yticks([])
-	# plt.ylabel('margin=8')
-
-	# plt.subplot(5, 2, 8)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_percussive8, ref=rp), y_axis='log')
-	# plt.yticks([]), plt.ylabel('')
-
-	# plt.subplot(5, 2, 9)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_harmonic16, ref=rp), y_axis='log')
-	# plt.yticks([])
-	# plt.ylabel('margin=16')
-
-	# plt.subplot(5, 2, 10)
-	# librosa.display.specshow(librosa.amplitude_to_db(D_percussive16, ref=rp), y_axis='log')
-	# plt.yticks([]), plt.ylabel('')
-
-	plt.tight_layout()
-	plt.show()
+		plt.tight_layout()
+		plt.show()
 
 if __name__ == '__main__':
-	main()
+	filename = '/Users/lucyzhang/Github/music-drawer-browser/static/uploads/penguindrum.wav'
+	decomposeAudio(filename)
+
+
+
+
+
+
